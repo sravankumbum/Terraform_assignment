a81a498cc58ee5fcb5449f1f817c28184fd2ceb6
sravankumbum <sravankumarreddy.ncs@gmail.com>
2026-04-27 14:59:19 +0530

    first commit

---
+++ 1/main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 (us-east-1)
+  instance_type = "t2.micro"
+
+  key_name = var.key_name
+
+  security_groups = [aws_security_group.app_sg.name]
+
+  user_data = file("user_data.sh")
+
+  tags = {
+    Name = "Flask-Express-Server"
+  }
+}
+
+resource "aws_security_group" "app_sg" {
+  name = "app-security-group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
